c10ec94610beedc1a64f8ace8289e60a413ae3a6
Tim <nebukam@gmail.com>
2019-09-29 23:59:36 -0400

    Samples update

--- Samples~/CylindricCluster/CylindricBrain.cs
+++ Samples~/CylindricCluster/CylindricBrain.cs
@@ -1,9 +1,29 @@
-﻿using Unity.Burst;
-using System.Runtime.CompilerServices;
-using UnityEngine;
-using Unity.Mathematics;
-using static Unity.Mathematics.math;
+﻿// Copyright (c) 2019 Timothé Lapetite - nebukam@gmail.com.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using Nebukam.Utils;
+using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Mathematics;
+using UnityEngine;
+using static Unity.Mathematics.math;
 
 namespace Nebukam.Cluster
 {
@@ -239,7 +259,7 @@ namespace Nebukam.Cluster
 
         private void UpdateBounds()
         {
-            float d = (((m_slotSize.x * m_clusterSize.x) / Maths.TAU) + m_slotSize.z * m_clusterSize.z )* 2f;
+            float d = (((m_slotSize.x * m_clusterSize.x) / Maths.TAU) + m_slotSize.z * m_clusterSize.z) * 2f;
             float3 p = m_pos;
             p.y += (m_slotSize.y * m_clusterSize.y) * 0.5f;
             m_bounds = new Bounds(p, float3(d, m_clusterSize.y * m_slotSize.y, d));
@@ -268,7 +288,7 @@ namespace Nebukam.Cluster
                 r = (m_slotSize.x * m_clusterSize.x) / Maths.TAU,
                 rMax = r + m_slotSize.z * m_clusterSize.z;
 
-            if(dist < r || dist > rMax)
+            if (dist < r || dist > rMax)
             {
                 coord = ByteTrio.zero;
                 return false;
@@ -276,7 +296,7 @@ namespace Nebukam.Cluster
 
             float a = atan2(loc.x, loc.z);
             a = a < 0f ? a + Maths.TAU : a;
-            loc.x = (m_slotSize.x * m_clusterSize.x) * ( a / Maths.TAU );
+            loc.x = (m_slotSize.x * m_clusterSize.x) * (a / Maths.TAU);
             loc.z = dist - r;
 
             float
@@ -288,7 +308,7 @@ namespace Nebukam.Cluster
                 posX = (int)((loc.x - modX) / m_slotSize.x),
                 posY = (int)((loc.y - modY) / m_slotSize.y),
                 posZ = (int)((loc.z - modZ) / m_slotSize.z);
-            
+
             coord = Clamp(posX, posY, posZ);
             return true;
 
@@ -303,11 +323,11 @@ namespace Nebukam.Cluster
         public float3 ComputePosition(ref ByteTrio coords)
         {
 
-            float 
+            float
                 r = (m_slotSize.x * m_clusterSize.x) / Maths.TAU,
-                a = (Maths.TAU / m_clusterSize.x ) * coords.x;
+                a = (Maths.TAU / m_clusterSize.x) * coords.x;
 
-            float3 
+            float3
                 p = coords * m_slotSize + m_slotOffset,
                 pt = float3(0f, p.y, r + m_slotSize.z * coords.z);
 
